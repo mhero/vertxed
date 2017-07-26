58aa55c3324d1edae9978144c74bbf7fbdcef84b
Marco <marco-aviles@hotmail.com>
2017-07-26 01:33:37 -0500

    Stage CRUD for movies db using mongodb

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vertxed
+# vertxed <a href="https://snyk.io/test/github/mhero/vertxed?targetFile=rx-rest%2Fpom.xml"><img src="https://snyk.io/test/github/mhero/vertxed/badge.svg?targetFile=rx-rest%2Fpom.xml" alt="Known Vulnerabilities" data-canonical-src="https://snyk.io/test/github/mhero/vertxed?targetFile=rx-rest%2Fpom.xml" style="max-width:100%;"></a> <a href="https://codeclimate.com/github/mhero/vertxed/maintainability"><img src="https://api.codeclimate.com/v1/badges/2ff61864555f6a504504/maintainability" /></a> <a href="https://codeclimate.com/github/mhero/vertxed/test_coverage"><img src="https://api.codeclimate.com/v1/badges/2ff61864555f6a504504/test_coverage" /></a>
+
 small REST api using vertx
---
+++ rx-rest/src/main/java/com/mac/rx/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mac.rx;
+
+import io.vertx.core.Vertx;
+
+/**
+ *
+ * @author marco
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        if (args.length == 0) {
+            vertx.deployVerticle(new RestVerticle(8080));
+        }
+
+        for (String arg : args) {
+            try {
+                Integer port = Integer.parseInt(arg);
+                vertx.deployVerticle(new RestVerticle(port));
+            } catch (NumberFormatException nfe) {
+                System.out.println("bad pot info:" + arg);
+            }
+        }
+    }
+
+}
