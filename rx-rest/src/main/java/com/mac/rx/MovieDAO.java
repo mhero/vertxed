66e5cda7079231458f9f6efa2cd79fe04a81923b
Marco <marco-aviles@hotmail.com>
2017-07-26 02:30:23 -0500

    moving crud functionality to DAO class

---
+++ rx-rest/src/main/java/com/mac/rx/MovieDAO.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mac.rx;
+
+import static com.mac.rx.MyFirstVerticle.COLLECTION;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.RoutingContext;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author marco
+ */
+public class MovieDAO {
+
+    private MongoClient mongo;
+
+    public MovieDAO(MongoClient mongo) {
+        this.mongo = mongo;
+    }
+
+    public void addOne(RoutingContext routingContext) {
+        final Movie whisky = Json.decodeValue(routingContext.getBodyAsString(),
+                Movie.class);
+
+        mongo.insert(COLLECTION, whisky.toJson(), r
+                -> routingContext.response()
+                        .setStatusCode(201)
+                        .putHeader("content-type", "application/json; charset=utf-8")
+                        .end(Json.encodePrettily(whisky.setId(r.result()))));
+    }
+
+    public void getOne(RoutingContext routingContext) {
+        final String id = routingContext.request().getParam("id");
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            mongo.findOne(COLLECTION, new JsonObject().put("_id", id), null, ar -> {
+                if (ar.succeeded()) {
+                    if (ar.result() == null) {
+                        routingContext.response().setStatusCode(404).end();
+                        return;
+                    }
+                    Movie whisky = new Movie(ar.result());
+                    routingContext.response()
+                            .setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
+                            .end(Json.encodePrettily(whisky));
+                } else {
+                    routingContext.response().setStatusCode(404).end();
+                }
+            });
+        }
+    }
+
+    public void updateOne(RoutingContext routingContext) {
+        final String id = routingContext.request().getParam("id");
+        JsonObject json = routingContext.getBodyAsJson();
+        if (id == null || json == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            mongo.update(COLLECTION,
+                    new JsonObject().put("_id", id),
+                    new JsonObject()
+                            .put("$set", json),
+                    v -> {
+                        if (v.failed()) {
+                            routingContext.response().setStatusCode(404).end();
+                        } else {
+                            routingContext.response()
+                                    .putHeader("content-type", "application/json; charset=utf-8")
+                                    .end(Json.encodePrettily(new Movie(id, json.getString("name"), json.getString("rate"))));
+                        }
+                    });
+        }
+    }
+
+    public void deleteOne(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            mongo.removeOne(COLLECTION, new JsonObject().put("_id", id),
+                    ar -> routingContext.response().setStatusCode(204).end());
+        }
+    }
+
+    public void getAll(RoutingContext routingContext) {
+        mongo.find(COLLECTION, new JsonObject(), results -> {
+            List<JsonObject> objects = results.result();
+            List<Movie> movies = objects.stream().map(Movie::new).collect(Collectors.toList());
+            routingContext.response()
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    .end(Json.encodePrettily(movies));
+        });
+    }
+
+    public void createSomeData(Handler<AsyncResult<Void>> next, Future<Void> fut) {
+        Movie bowmore = new Movie("The Shawshank Redemption", "9.3");
+        Movie talisker = new Movie("The Godfather", "9.2");
+        System.out.println(bowmore.toJson());
+
+        mongo.count(COLLECTION, new JsonObject(), count -> {
+            if (count.succeeded()) {
+                if (count.result() == 0) {
+
+                    mongo.insert(COLLECTION, bowmore.toJson(), ar -> {
+                        if (ar.failed()) {
+                            fut.fail(ar.cause());
+                        } else {
+                            mongo.insert(COLLECTION, talisker.toJson(), ar2 -> {
+                                if (ar2.failed()) {
+                                    fut.failed();
+                                } else {
+                                    next.handle(Future.<Void>succeededFuture());
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    next.handle(Future.<Void>succeededFuture());
+                }
+            } else {
+                // report the error
+                fut.fail(count.cause());
+            }
+        });
+    }
+}
--- rx-rest/src/main/java/com/mac/rx/MyFirstVerticle.java
+++ rx-rest/src/main/java/com/mac/rx/MyFirstVerticle.java
@@ -7,31 +7,29 @@ import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import java.util.HashSet;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
     public static final String COLLECTION = "movies";
     private MongoClient mongo;
+    private MovieDAO movieDao;
 
     @Override
     public void start(Future<Void> fut) {
 
         mongo = MongoClient.createShared(vertx, config());
+        movieDao = new MovieDAO(mongo);
 
-        createSomeData(
+        movieDao.createSomeData(
                 (nothing) -> startWebApp(
                         (http) -> completeStartup(http, fut)
                 ), fut);
@@ -66,11 +64,12 @@ public class MyFirstVerticle extends AbstractVerticle {
         router.route("/api/movies*").handler(CorsHandler.create("*")
                 .allowedHeaders(allowHeaders)
                 .allowedMethods(allowMethods));
-        router.get("/api/movies").handler(this::getAll);
-        router.post("/api/movies").handler(this::addOne);
-        router.get("/api/movies/:id").handler(this::getOne);
-        router.put("/api/movies/:id").handler(this::updateOne);
-        router.delete("/api/movies/:id").handler(this::deleteOne);
+
+        router.get("/api/movies").handler(movieDao::getAll);
+        router.post("/api/movies").handler(movieDao::addOne);
+        router.get("/api/movies/:id").handler(movieDao::getOne);
+        router.put("/api/movies/:id").handler(movieDao::updateOne);
+        router.delete("/api/movies/:id").handler(movieDao::deleteOne);
 
         vertx
                 .createHttpServer()
@@ -94,110 +93,4 @@ public class MyFirstVerticle extends AbstractVerticle {
         mongo.close();
     }
 
-    private void addOne(RoutingContext routingContext) {
-        final Movie whisky = Json.decodeValue(routingContext.getBodyAsString(),
-                Movie.class);
-
-        mongo.insert(COLLECTION, whisky.toJson(), r
-                -> routingContext.response()
-                        .setStatusCode(201)
-                        .putHeader("content-type", "application/json; charset=utf-8")
-                        .end(Json.encodePrettily(whisky.setId(r.result()))));
-    }
-
-    private void getOne(RoutingContext routingContext) {
-        final String id = routingContext.request().getParam("id");
-        if (id == null) {
-            routingContext.response().setStatusCode(400).end();
-        } else {
-            mongo.findOne(COLLECTION, new JsonObject().put("_id", id), null, ar -> {
-                if (ar.succeeded()) {
-                    if (ar.result() == null) {
-                        routingContext.response().setStatusCode(404).end();
-                        return;
-                    }
-                    Movie whisky = new Movie(ar.result());
-                    routingContext.response()
-                            .setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
-                            .end(Json.encodePrettily(whisky));
-                } else {
-                    routingContext.response().setStatusCode(404).end();
-                }
-            });
-        }
-    }
-
-    private void updateOne(RoutingContext routingContext) {
-        final String id = routingContext.request().getParam("id");
-        JsonObject json = routingContext.getBodyAsJson();
-        if (id == null || json == null) {
-            routingContext.response().setStatusCode(400).end();
-        } else {
-            mongo.update(COLLECTION,
-                    new JsonObject().put("_id", id),
-                    new JsonObject()
-                            .put("$set", json),
-                    v -> {
-                        if (v.failed()) {
-                            routingContext.response().setStatusCode(404).end();
-                        } else {
-                            routingContext.response()
-                                    .putHeader("content-type", "application/json; charset=utf-8")
-                                    .end(Json.encodePrettily(new Movie(id, json.getString("name"), json.getString("rate"))));
-                        }
-                    });
-        }
-    }
-
-    private void deleteOne(RoutingContext routingContext) {
-        String id = routingContext.request().getParam("id");
-        if (id == null) {
-            routingContext.response().setStatusCode(400).end();
-        } else {
-            mongo.removeOne(COLLECTION, new JsonObject().put("_id", id),
-                    ar -> routingContext.response().setStatusCode(204).end());
-        }
-    }
-
-    private void getAll(RoutingContext routingContext) {
-        mongo.find(COLLECTION, new JsonObject(), results -> {
-            List<JsonObject> objects = results.result();
-            List<Movie> movies = objects.stream().map(Movie::new).collect(Collectors.toList());
-            routingContext.response()
-                    .putHeader("content-type", "application/json; charset=utf-8")
-                    .end(Json.encodePrettily(movies));
-        });
-    }
-
-    private void createSomeData(Handler<AsyncResult<Void>> next, Future<Void> fut) {
-        Movie bowmore = new Movie("The Shawshank Redemption", "9.3");
-        Movie talisker = new Movie("The Godfather", "9.2");
-        System.out.println(bowmore.toJson());
-
-        mongo.count(COLLECTION, new JsonObject(), count -> {
-            if (count.succeeded()) {
-                if (count.result() == 0) {
-
-                    mongo.insert(COLLECTION, bowmore.toJson(), ar -> {
-                        if (ar.failed()) {
-                            fut.fail(ar.cause());
-                        } else {
-                            mongo.insert(COLLECTION, talisker.toJson(), ar2 -> {
-                                if (ar2.failed()) {
-                                    fut.failed();
-                                } else {
-                                    next.handle(Future.<Void>succeededFuture());
-                                }
-                            });
-                        }
-                    });
-                } else {
-                    next.handle(Future.<Void>succeededFuture());
-                }
-            } else {
-                // report the error
-                fut.fail(count.cause());
-            }
-        });
-    }
 }
