c7c21e0ac50e6015113a0a4cc77d2179c0efa134
mhero <gdmarav374@gmail.com>
2020-11-21 17:36:58 -0500

    Refactor routes into its own class

--- src/main/java/com/mac/rx/verticle/RestVerticle.java
+++ src/main/java/com/mac/rx/verticle/RestVerticle.java
@@ -7,13 +7,8 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
-import io.vertx.ext.web.handler.CorsHandler;
-import io.vertx.ext.web.handler.StaticHandler;
 
 public class RestVerticle extends AbstractVerticle {
 
@@ -29,35 +24,19 @@ public class RestVerticle extends AbstractVerticle {
 	}
 
 	@Override
-	public void start(Promise<Void> fut) {
+	public void start(Promise<Void> promise) {
 
 		mongo = MongoClient.createShared(vertx, mongoConfig);
 		movieDao = new MovieDAO(mongo);
 
-		movieDao.createSomeData((nothing) -> startWebApp((http) -> completeStartup(http, fut)), fut);
+		movieDao.createFixture((nothing) -> startWebApp((http) -> completeStartup(http, promise)), promise);
 	}
 
 	private void startWebApp(Handler<AsyncResult<HttpServer>> next) {
-		Router router = Router.router(vertx);
 
-		router.route("/").handler(routingContext -> {
-			HttpServerResponse response = routingContext.response();
-			response.putHeader("content-type", "text/html").end("<h1>Hello Vert.x 3!</h1>");
-		});
-
-		router.route("/assets/*").handler(StaticHandler.create("assets"));
-
-		router.route("/api/movies*").handler(BodyHandler.create());
-		router.route("/api/movies*").handler(CorsHandler.create("*").allowedHeaders(Headers.allowedHeaders)
-				.allowedMethods(HttpMethods.allowedMethods));
-		router.get("/api/movies").handler(movieDao::getAll);
-		router.post("/api/movies").handler(movieDao::addOne);
-		router.get("/api/movies/:id").handler(movieDao::getOne);
-		router.put("/api/movies/:id").handler(movieDao::updateOne);
-		router.delete("/api/movies/:id").handler(movieDao::deleteOne);
-
-		vertx.createHttpServer().requestHandler(router).listen(config().getInteger("http.port", this.port),
-				next::handle);
+		Routes routes = new Routes();
+		vertx.createHttpServer().requestHandler(routes.createRoutes(vertx, movieDao))
+				.listen(config().getInteger("http.port", this.port), next::handle);
 	}
 
 	private void completeStartup(AsyncResult<HttpServer> http, Promise<Void> fut) {
