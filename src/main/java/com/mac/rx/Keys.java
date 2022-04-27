53c7163e5002c5df1206f73cfd1247edc7c739bb
Marco <gdmarav374@gmail.com>
2022-04-27 10:12:08 -0500

    Include keystore generator

---
+++ src/main/java/com/mac/rx/Keys.java
@@ -0,0 +1,25 @@
+package com.mac.rx;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+public class Keys {
+	public Keys() {
+		try {
+			KeyStore ks = KeyStore.getInstance("jceks");
+			char[] pwdArray = "secret".toCharArray();
+			ks.load(null, pwdArray);
+
+			try (FileOutputStream fos = new FileOutputStream("keystore.jceks")) {
+				ks.store(fos, pwdArray);
+			}
+		} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
